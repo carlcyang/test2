e879fa525e6730382fab5e59aa1c3a6aea1d6e0e
carlcyang <805821966@qq.com>
2024-05-15 19:42:37 +0800

    Update CodeHelper.java
    1

--- CodeHelper.java
+++ CodeHelper.java
@@ -3,10 +3,10 @@ public class CodeHelper {
     public static String extractCode(String originalCode) {
         String html = FlexMarkProcessor.markdownToHtml(originalCode);
         Document document = Jsoup.parseBodyFragment(html);
-        Elements elements = document.select("pre > code");
-        for (Element element : elements) {
-            // String language = element.attr("class");
-            List<TextNode> textNodes = element.textNodes();
+        Elements elements2 = document.select("pre > code");
+        for (Element element1 : elements2) {
+            // String language = element1.attr("class");
+            List<TextNode> textNodes = element1.textNodes();
             return !textNodes.isEmpty() ? textNodes.get(0).getWholeText().trim() : null;
         }
         return null;
